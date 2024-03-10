bee08e9f3e25bd1b8f889b9cda33d22fe15fa274
e.gholami <e.gholami@behsacorp.com>
2024-03-10 11:37:25 +0330

    Extract username from token with JWTS libraries

--- src/main/java/com/th3arock/Spring/Security/config/JwtService.java
+++ src/main/java/com/th3arock/Spring/Security/config/JwtService.java
@@ -1,12 +1,40 @@
 package com.th3arock.Spring.Security.config;
 
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "c504ccd074577907f97538ff5dc708795ec00eecc90d46571989c9e692ccaa8d";
+
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token , Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
